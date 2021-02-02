f9b8e38a261745f793b1e3eb3914c87afc5523c0
moham <moham@UNDERCOVERFLY>
2021-02-02 11:01:49 +0530

    demo

---
+++ src/com/capgemini/beans/Employee.java
@@ -0,0 +1,22 @@
+package com.capgemini.beans;
+
+public class Employee {
+
+private int empNumber;
+private String Name;
+public int getEmpNumber() {
+	return empNumber;
+}
+public void setEmpNumber(int empNumber) {
+	this.empNumber = empNumber;
+}
+public String getName() {
+	return Name;
+}
+public void setName(String name) {
+	Name = name;
+}
+
+	}
+
+
